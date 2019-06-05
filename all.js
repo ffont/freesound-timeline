ce8f4a0b4052718a4caa8e1526b5f681f898b256
ffont <frederic.font@upf.edu>
2019-06-05 13:14:06 +0200

    Sync to month/year on change

--- all.js
+++ all.js
@@ -405,6 +405,12 @@ function parseHashAndSetParams() {
     }
 }
 
+function yearChange(){
+    var element = document.getElementById('year');
+    setHash();
+    play();  // Sync to current date
+}
+
 function monthChange(){
     var element = document.getElementById('month');
     if (element.value < 1){
@@ -414,6 +420,7 @@ function monthChange(){
         element.value = 12;
     }
     setHash();
+    play();  // Sync to current date
 }
 
 function setHash() {
